5e0a5e2f14b140b08f88c8cb45f3ffabbeb73b73
A.Bond <alexey.bond.94.55@gmail.com>
2016-12-11 18:09:47 +0600

    fixx

--- gm/item_types.py
+++ gm/item_types.py
@@ -2,6 +2,6 @@ from shr import *
 
 ITEMS = {
     'ogurezz': ItemType('ogurezz', {'energy': '{0}+.1'}, 'rc/img/banka.png'),
-    'ogurezz1': ItemType('ogurezz', {'energy': '{0}+.1'}, 'rc/img/banka.png'),
-    'ogurezz2': ItemType('ogurezz', {'energy': '{0}+.1'}, 'rc/img/banka.png')
+    'ogurezz1': ItemType('ogurezz', {'energy': '{0}-.2'}, 'rc/img/banka2.png'),
+    'ogurezz2': ItemType('ogurezz', {'energy': '{0}+.3'}, 'rc/img/banka3.png')
 }
